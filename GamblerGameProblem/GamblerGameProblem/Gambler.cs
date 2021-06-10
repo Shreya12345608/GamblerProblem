fb3e760a220c4cb0be02a0230939a2a753af0d68
Shreya Malviya <malviyashreya26@gmail.com>
2021-06-10 12:57:42 +0530

    [Shreya] Add: Set A value in Glamber

---
+++ GamblerGameProblem/GamblerGameProblem/Gambler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblerGameProblem
+{ 
+    //class  Gambler
+
+    class Gambler
+    {
+        //Instance Variable
+        const int STACKE = 100;
+        const int BET = 1;
+
+    }
+}
---
+++ GamblerGameProblem/GamblerGameProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblerGameProblem
+{
+    //class  Gambler
+   
+        //Main Program
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Welcome to gambling solution");
+                Console.ReadLine();
+            }
+        }
+    }
+}
